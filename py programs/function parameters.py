5618d0cf620427b59e8e3756ec4d88e7f0c54758
Keerthivasan P <121535522+KeerthivsasanP@users.noreply.github.com>
2023-10-25 20:18:13 -0400

    function parameters

---
+++ py programs/function parameters.py
@@ -0,0 +1,7 @@
+def greet(name,location):
+    print(f"Hello {name} from {location}")
+    
+greet("Ram","Ayodhi")
+greet(location="Mithilai",name='Seetha')
+
+#greet(location="Mithilai","Lakshman")  //Provides syntax error
