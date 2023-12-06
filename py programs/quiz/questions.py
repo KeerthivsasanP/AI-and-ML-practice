cef3060f9bdfc640a258632ec6e9c674575c254d
Keerthivasan P <121535522+KeerthivsasanP@users.noreply.github.com>
2023-12-06 16:42:33 +0530

    turtle try

--- py programs/quiz/questions.py
+++ py programs/quiz/questions.py
@@ -4,9 +4,11 @@ class Question:
     def __init__(self,text,answer):
         self.text = text
         self.answer = answer
+        
+    def check_answer(self,user_answer):
+        return self.answer.lower() == user_answer.lower()
 
 quiz = []
-question_number=0
 total_score = 0
 
 for question in data:
@@ -20,8 +22,9 @@ print("Welcome to the quiz")
 
 for qno in range(0,total_question):
     user_ans = input(f"Q{qno+1} . {quiz[qno].text} (True/False)")
-    if user_ans == quiz[qno].answer:
+    if Question.check_answer(quiz[qno],user_ans):
         total_score += 1
+    print(f"Correct answer is {quiz[qno].answer}\nYour current score {total_score}")
 
 print(f"Your score = {total_score}")
    
